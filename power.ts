a3a6a997b720ff4a79c0cf0f9a7a072b1813fc1d
ABDULLAHALAMIEN <m-12681062@moe-dl.edu.my>
2025-10-17 06:40:18 +0800

    Add power management functions to power.ts

---
+++ power.ts
@@ -0,0 +1,72 @@
+
+
+namespace makerDrive {
+    /**
+     * Return true if power is on.
+     */
+    //% weight=20
+    //% blockGap=8
+    //% blockId=rekabit_is_power_on
+    //% block="power on"
+    //% blockHidden=true
+    export function isPowerOn(): boolean {
+        if (makerDrive.i2cRead(REG_ADD_PWR_STATE) != 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+
+    /**
+     * Return true if low batt.
+     */
+    //% weight=19
+    //% blockGap=8
+    //% blockId=rekabit_is_low_batt
+    //% block="low batt"
+    //% blockHidden=true
+    export function isLowBatt(): boolean {
+        if (makerDrive.i2cRead(REG_ADD_LB_STATE) != 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+
+    /**
+     * Return true if overvoltage.
+     */
+    //% weight=18
+    //% blockGap=40
+    //% blockId=rekabit_is_overvoltage
+    //% block="overvoltage"
+    //% blockHidden=true
+    export function isOvervoltage(): boolean {
+        if (makerDrive.i2cRead(REG_ADD_OV_STATE) != 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+
+    /**
+     * Return power input voltage.
+     */
+    //% weight=17
+    //% blockGap=8
+    //% blockId=rekabit_read_vin
+    //% block="power input voltage"
+    //% blockHidden=true
+    export function readVin(): number {
+        return (makerDrive.i2cRead(REG_ADD_VIN) / 10);
+    }
+}
